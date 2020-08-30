08df92a2ff9eb99acb81292988e7f355a3d0da39
Anthony  GT <anthonygt124@gmail.com>
2020-08-30 20:56:24 +0700

    Remove type="submit" on verify email button

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -12,7 +12,7 @@
         <div class="inner-border text-center">
             <article class="text-white font-weight-bold">Halo !</article>
             <p class="notify text-white">Untuk mendaftarkan diri pada lomba SPSS 2020, harap melakukan verifikasi e-mail terlebih dahulu dengan cara mengecek e-mail anda.</p>
-            <a href="{{ route('verification.resend') }}" class="text-success" type="submit"
+            <a href="{{ route('verification.resend') }}" class="text-success"
                 onclick="event.preventDefault(); document.getElementById('email-resend-form').submit();">
                 Klik ini untuk mengirim e-mail verifikasi kembali
             </a>
